f3920165fca411049734e17f29163176944b74bd
marcia yurie yano <83242742+marciayano@users.noreply.github.com>
2022-02-24 20:01:52 -0300

    aula 3

---
+++ my-app/src/App.js
@@ -0,0 +1,23 @@
+//Importando o nosso componente Box;
+import Box from './Box';
+
+//Criação do componente em formato de função.
+function App() {
+
+  const frutas = ["Cachorro", "Gato", "Macaco"];
+
+  return (
+    <>
+    {
+      frutas.map((item, index)=>{
+        return(
+          // <li key={index.toString()}>{item}</li>
+          <Box item={item} key={index.toString()}/>
+        )
+      })
+    }
+    </>
+  )
+}
+
+export default App;
